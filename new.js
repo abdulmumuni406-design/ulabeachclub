eca518c5fad89523f51e0b4a312c75ebc7b0c18a
unknown <abdulmumuni406@gmail.com>
2026-06-21 20:38:34 +0000

    new changes

---
+++ new.js
@@ -0,0 +1,8 @@
+const age = 16
+const text = (age <= 18) ? "Minor" : "Adult";
+document.getElementById ("demo").innerHTML = text;
+
+// const age = 16;
+// const text = (age <= 18) ? "Minor" : "Adult";
+
+// document.getElementById("demo").innerHTML = text;
