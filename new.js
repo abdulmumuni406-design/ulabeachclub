543aeaf88d1ae119945685b41e96ef9bc7752ade
unknown <abdulmumuni406@gmail.com>
2026-06-24 15:04:43 +0000

    new work

--- new.js
+++ new.js
@@ -1,8 +1,20 @@
-const age = 16
-const text = (age <= 18) ? "Minor" : "Adult";
-document.getElementById ("demo").innerHTML = text;
+let day;
+let date = new Date().getDay();
 
-// const age = 16;
-// const text = (age <= 18) ? "Minor" : "Adult";
+if (date == 0) {
+    day = "Sunday";
+} else if (date == 1) {
+    day = "Monday";
+} else if (date == 2) {
+    day = "Tuesday";
+} else if (date == 3) {
+    day = "Wednesday";
+} else if (date == 4) {
+    day = "Thursday";
+} else if (date == 5) {
+    day = "Friday";
+} else if (date == 6) {
+    day = "Saturday";
+}
 
-// document.getElementById("demo").innerHTML = text;
+document.getElementById("demo").innerHTML = day;
